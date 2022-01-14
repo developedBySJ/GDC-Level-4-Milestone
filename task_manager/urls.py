fba36f58f98df582df876da5a9d1f6163aefe08b
developedBySJ <developedbysj@gmail.com>
2022-01-14 16:32:14 +0530

    Complete level 4 milestone

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 
+from tasks.views import add_task, complete_task, completed_task_view, delete_task, home_view, task_view
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Add all your views here
+    path('', home_view),
+    path('tasks', task_view),
+    path('pending_tasks', task_view),
+    path('all_tasks', home_view),
+    path('completed_tasks', completed_task_view),
+    path('add-task', add_task),
+    path('complete_task/<int:index>', complete_task),
+    path('delete-task/<int:index>', delete_task),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,34 @@
+from django.http.response import HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
 # Add all your views here
+
+
+def add_task(request):
+    todo = request.GET.get("task")
+    tasks.append({"todo": todo, "is_completed": False})
+    return HttpResponseRedirect("/tasks")
+
+
+def complete_task(request, index):
+    todo = tasks[index-1]
+    todo["is_completed"] = True
+    return HttpResponseRedirect("/tasks")
+
+
+def delete_task(request, index):
+    del tasks[index-1]
+    return HttpResponseRedirect("/tasks")
+
+
+def task_view(request):
+    return render(request, "task.html", {"path": "pending_tasks", "tasks": filter(lambda t: not t.get("is_completed"), tasks)})
+
+
+def home_view(request):
+    return render(request, "task.html", {"path": "all_tasks", "tasks": tasks})
+
+
+def completed_task_view(request):
+    return render(request, "task.html", {"path": "completed_tasks", "tasks": filter(lambda t: t.get("is_completed"), tasks)})
